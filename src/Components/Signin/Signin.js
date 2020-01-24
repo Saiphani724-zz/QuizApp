80872be9cb16d8e71686bc32a914a237d881297e
saiphani724 <saiphani724@gmail.com>
2020-01-24 11:03:01 +0530

    Username check added

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -6,6 +6,36 @@ import './Signin.css';
 import { withRouter } from 'react-router-dom'
 
 class Signin extends Component {
+	state = {
+		'username' : '',
+		'password' : '',
+		'wrongUsername' : false
+	}
+
+	handleUsername  = (e) => {
+		this.setState({
+			'username' : e.target.value,
+		})
+		// console.log(this.state);
+	}
+
+	handlePassword = (e)=>{
+		this.setState({
+			'password' : e.target.value,
+		})
+		// console.log(this.state);
+	}
+
+	checkRegex = ()=>{
+		if(this.state.username === "Phani"){
+			return true
+		}
+		else{
+			this.setState({'wrongUsername' : 1})
+			return false
+		}
+	}
+
 	render() {
 
 		const Button = withRouter(({ history }) => (
@@ -15,7 +45,8 @@ class Signin extends Component {
 				value="Sign In"
 				onClick={
 					() => {
-						history.push('/dashboard')
+						if(this.checkRegex())
+							history.push('/dashboard')
 					}
 				}
 			>
@@ -24,7 +55,6 @@ class Signin extends Component {
 		))
 
 		return (
-
 			<div>
 				{/* <form>
 
@@ -36,13 +66,18 @@ class Signin extends Component {
 					<div class="form-group row">
 						<label for="username" class="col-sm-2 col-form-label">Username</label>
 						<div class="col-sm-6">
-							<input type="username" class="form-control" id="username" placeholder="Enter your Username" />
+							<input type="username" class="form-control" id="username" placeholder="Enter your Username" onChange={this.handleUsername} />
 						</div>
 					</div>
 					<div class="form-group row">
 						<label for="password" class="col-sm-2 col-form-label">Password</label>
 						<div class="col-sm-6">
-							<input type="password" class="form-control" id="password" placeholder="Enter your Password" />
+							<input type="password" class="form-control" id="password" placeholder="Enter your Password" onChange={this.handlePassword}/>
+							{
+								this.state.wrongUsername  ? 
+									<p>WRONG USERNAME FORMAT</p> 
+									: null
+							} 
 						</div>
 					</div>
 				</form>
