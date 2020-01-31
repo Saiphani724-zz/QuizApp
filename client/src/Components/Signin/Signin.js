eeb632ef5a5926ce165f0d305895965bf1b9cfbb
saiphani724 <saiphani724@gmail.com>
2020-02-01 01:11:59 +0530

    Express connected

---
+++ client/src/Components/Signin/Signin.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import './Signin.css';
+// import Input from '../UI/Input/Input';
+// import '../UI/Input/Input.css';
+import axios from 'axios';
+import {
+	withRouter,
+	// BrowserRouter
+} from 'react-router-dom'
+
+class Signin extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: "",
+			password: ""
+		}
+	}
+	componentDidMount() {
+	}
+
+	onUsernameChanged = (e) => {
+		this.setState({ username: e.target.value })
+	}
+	onPasswordChanged = (e) => {
+		this.setState({ password: e.target.value })
+	}
+
+	verifyUserLogin = (history) => {
+		if (this.state.userFound) {
+			history.push('/dashboard')
+		}
+		else {
+			console.log(this.state.userFound);
+		}
+	}
+
+	handleSignin = (history) => {
+		fetch(`login?username=${this.state.username}&&password=${this.state.password}`, {
+			method: 'GET',
+		}).then(res => res.json())
+			.then(data => {
+				this.setState(
+					() => (data),
+					function () {
+						this.verifyUserLogin(history);
+					}
+				)
+			})
+	}
+
+	render() {
+		const Button = withRouter(({ history }) => (
+			<button
+				className="btn-lg btn btn-dark indent"
+				type='submit'
+				value="Sign In"
+				onClick={() => { this.handleSignin(history) }}
+			>
+				Sign In
+			</button>
+		))
+
+		return (
+			<div>
+				<form>
+					<div class="form-group row">
+						<label for="username" class="col-sm-2 col-form-label">Username</label>
+						<div class="col-sm-6">
+							<input type="username" class="form-control" id="username" placeholder="Enter your Username" onChange={this.onUsernameChanged} />
+						</div>
+
+					</div>
+					<div class="form-group row">
+						<label for="password" class="col-sm-2 col-form-label">Password</label>
+						<div class="col-sm-6">
+							<input type="password" class="form-control" id="password" placeholder="Enter your Password" onChange={this.onPasswordChanged} />
+						</div>
+					</div>
+				</form>
+				{
+					this.state.userFound === false ?
+						<p class="indentErrorMsg">User doesn't exist or Wrong Password</p>
+						: null
+				}
+
+				<div class="form-group row">
+					<div class="col-sm-6 offset-sm-2">
+						<Button />
+					</div>
+				</div>
+			</div>
+		);
+	}
+}
+
+export default Signin;
